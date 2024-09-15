6f72ba54cc61812c2b8fd2de747309ba0b88f75f
Oloruntobi600 <tobiajayi60@gmail.com>
2024-09-15 06:07:14 +0100

    device responsiveness

--- src/component/Home/MultiItemCarousel.jsx
+++ src/component/Home/MultiItemCarousel.jsx
@@ -4,6 +4,7 @@ import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import { topMeels } from "./topMeels";
 import CarouselItem from "./CarouselItem";
+import "./MultiItemCarousel.css";
 
 const MultiItemCarousel = () => {
     const settings = {
@@ -14,13 +15,30 @@ const MultiItemCarousel = () => {
         slidesToScroll: 1,
         autoplay:true,
         autoplaySpeed:2000,
-        arrows: false
+        arrows: false,
+        responsive: [
+            {
+              breakpoint: 1024, // For tablets and smaller devices
+              settings: {
+                slidesToShow: 3,
+              },
+            },
+            {
+              breakpoint: 768, // For mobile devices
+              settings: {
+                slidesToShow: 2,
+              },
+            },
+            {
+              breakpoint: 480, // For very small mobile devices
+              settings: {
+                slidesToShow: 1,
+              },
+            },
+          ],
       };
     return (
         <div>
-            {/* <Slider {...settings}>
-                {topMeels.map((item) =><CarouselItem image={item.image} title={item.title}/>)}
-            </Slider> */}
              <Slider {...settings}>
                 {topMeels.map((item, index) => (
                     <CarouselItem
