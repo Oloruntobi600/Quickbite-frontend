1dc94b830676e510d942e19d8c1c644d363c1dce
Oloruntobi600 <tobiajayi60@gmail.com>
2024-09-15 05:24:54 +0100

    axios update

--- src/component/config/api.js
+++ src/component/config/api.js
@@ -1,7 +1,10 @@
 // config/api.js
 import axios from "axios";
 
-export const API_URL = "http://localhost:8080"
+// export const API_URL = "http://localhost:8080"
+const API_URL = process.env.NODE_ENV === 'production' 
+  ? "https://quickbite-backend-58ik.onrender.com"  // Replace this with your actual deployed backend URL
+  : "http://localhost:8080";  // Local development backend URL
 
 export const api=axios.create({
   baseURL: API_URL,
