7119af2bfba7347bdf2b8d8c6bf3efa2ccc3f9a7
Oloruntobi600 <tobiajayi60@gmail.com>
2024-09-15 05:44:12 +0100

    backend origin

--- src/component/config/api.js
+++ src/component/config/api.js
@@ -1,7 +1,8 @@
 // config/api.js
 import axios from "axios";
 
-export const API_URL = "http://localhost:8080"
+// export const API_URL = "http://localhost:8080"
+export const API_URL = "https://quickbite-backend-58ik.onrender.com"
 
 export const api=axios.create({
   baseURL: API_URL,
