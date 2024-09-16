97a83aa6f829418821b37abcb265b0037b699827
Oloruntobi600 <tobiajayi60@gmail.com>
2024-09-16 22:12:20 +0100

    admin sidebar drawer

--- src/AdminComponent/Admin/AdminSideBar.jsx
+++ src/AdminComponent/Admin/AdminSideBar.jsx
@@ -68,17 +68,18 @@ const AdminSideBar = ({handleClose}) => {
             anchor='left'
             sx={{ zIndex: 1 }}
         >
-            <div className='w-[70vw] lg:w-[20vw] h-screen flex flex-col justify-center text-xl space-y-[1.65rem]'>
-                {menu.map((item, i) => (
-                    <React.Fragment key={item.title}> {/* Added key here */}
-                        <div onClick={() => handleNavigate(item)} className='px-5 flex items-center gap-5 cursor-pointer'>
-                            {item.icon}
-                            <span>{item.title}</span>
-                        </div>
-                        {i !== menu.length - 1 && <Divider />}
-                    </React.Fragment>
-                ))}
+           <div className={`w-[${isSmallScreen ? '50vw' : '70vw'}] lg:w-[20vw] h-screen flex flex-col justify-center text-xl space-y-[1.65rem]`}>
+    {menu.map((item, i) => (
+        <React.Fragment key={item.title}>
+            <div onClick={() => handleNavigate(item)} className='px-5 flex items-center gap-5 cursor-pointer'>
+                {item.icon}
+                <span>{item.title}</span>
             </div>
+            {i !== menu.length - 1 && <Divider />}
+        </React.Fragment>
+    ))}
+</div>
+
         </Drawer>
       </>
     </div>
